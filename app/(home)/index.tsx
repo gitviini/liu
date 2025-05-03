462caeb11dbb151777274477cae98612eaf2aced
gitviini <gvinicius105@gmail.com>
2025-05-03 20:57:58 -0300

    Stack options created

---
+++ app/(home)/index.tsx
@@ -0,0 +1,7 @@
+import Container from "@/assets/components/Container";
+
+export default function Home(){
+    return(
+        <Container></Container>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
+import COLORS from "@/assets/static/colors";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown:false}} />;
+  return <Stack screenOptions={{ 
+    headerShadowVisible: false, 
+    headerTintColor: COLORS.foreground, 
+    headerStyle: { backgroundColor: COLORS.background }, 
+  }}>
+    <Stack.Screen name="index" options={{headerShown: false}} />
+    <Stack.Screen name="(autentication)/login" options={{title:"Entrar"}} />
+    <Stack.Screen name="(autentication)/signup" options={{title:"Cadastrar"}} />
+    <Stack.Screen name="(home)" options={{headerShown: false}} />
+  </Stack>
 }
